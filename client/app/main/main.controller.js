d583b045f0eb72e488a9ae08558214f31b350d8a
Ivan Djokic <ivan@vantageanalytics.com>
2017-03-05 14:26:32 -0500

    main controller logic for splitting out past and future medications.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,11 +2,13 @@
 
 angular.module('medicationReminderApp').controller('MainCtrl', function ($scope, $http, $window) {
 
-    var start = moment().format('MM/DD/YYYY'),
+    var ctl = this,
+        start = moment().format('MM/DD/YYYY'),
         end = moment().add(1, 'day').format('MM/DD/YYYY');
 
     $http.get('/api/medications?start=' + start + '&end=' + end).then(function (meds) {
-        $scope.meds = meds.data;
+      ctl.medications = meds.data;
+    });
     });
 
     $window.setInterval(function () {
@@ -14,6 +16,21 @@ angular.module('medicationReminderApp').controller('MainCtrl', function ($scope,
 
       $scope.currentDate = now.format('dddd MMMM Do');
       $scope.currentTime = now.format('h:mm a');
+
+      $scope.pastMedications = _.sortBy(
+      _.filter(ctl.medications, function(item) {
+        var med = moment(item.time);
+
+        return med.diff(now, 'minutes') < 0;
+      }), 'time').reverse();
+
+      $scope.futureMedications = _.sortBy(
+      _.filter(ctl.medications, function(item) {
+        var med = moment(item.time);
+
+        return med.diff(now, 'minutes') >= 0;
+      }), 'time');
+
       $scope.$apply();
     }, 1000);
 
