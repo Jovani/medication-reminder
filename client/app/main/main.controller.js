f941d41d243da5eb80874c3c6414b7fb1c9793e3
Ivan Djokic <ivan@vantageanalytics.com>
2017-03-06 01:10:16 -0500

    Main UI for observing, and "completing" medications. Includes alerts for medications, current and missed.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,33 +3,80 @@
 angular.module('medicationReminderApp').controller('MainCtrl', function ($scope, $http, $window) {
 
     var ctl = this,
-        start = moment().format('MM/DD/YYYY'),
-        end = moment().add(1, 'day').format('MM/DD/YYYY');
+        upcomingMedModal = Modal.confirm.upcomingMed(),
+        missedMedModal = Modal.confirm.missedMed();
 
-    $http.get('/api/medications?start=' + start + '&end=' + end).then(function (meds) {
-      ctl.medications = meds.data;
-    });
+    /*
+      Helper function to split the full array of medications
+      into more useable chunks. Namely, past, future, and complete.
+
+      It's not the most efficient function in the world. An obvious
+      improvement would be to make it iterate over the full array only once.
+    */
+    var splitMedications = function(now) {
+      $scope.pastMedications = _.sortBy(
+        _.filter(ctl.medications, function(med) {
+          if (med.completed) {
+            return false;
+          }
+
+          var medTime = moment(med.time);
+          return medTime.diff(now, 'minutes') < -5;
+        }), 'time').reverse();
+
+      $scope.futureMedications = _.sortBy(
+        _.filter(ctl.medications, function(med) {
+          if (med.completed) {
+            return false;
+          }
+
+          var medTime = moment(med.time);
+          return medTime.diff(now, 'minutes') >= -5;
+        }), 'time');
+
+      $scope.completeMedications = _.sortBy(
+        _.filter(ctl.medications, function(med) {
+          return med.completed;
+        }), 'time').reverse();
+    };
+
+    /*
+      Helper function to fetch medications for a given date.
+    */
+    $scope.refreshMedications = function(start, end) {
+      start = start || moment($scope.selectedDate).format('MM/DD/YYYY');
+      end = end || moment($scope.selectedDate).add(1, 'day').format('MM/DD/YYYY');
+
+      $http.get('/api/medications?start=' + start + '&end=' + end).then(function (meds) {
+        ctl.medications = meds.data;
+      });
+    };
+
     });
 
     $window.setInterval(function () {
-      var now = moment();
+      var now = moment(),
+          selectedDate = moment($scope.selectedDate);
 
-      $scope.currentDate = now.format('dddd MMMM Do');
+      $scope.currentDate = selectedDate.format('dddd MMMM Do');
       $scope.currentTime = now.format('h:mm a');
 
-      $scope.pastMedications = _.sortBy(
-      _.filter(ctl.medications, function(item) {
-        var med = moment(item.time);
+      splitMedications(now);
 
-        return med.diff(now, 'minutes') < 0;
-      }), 'time').reverse();
+      // Loop through incomplete meds and alert as appropriate.
+      var incompleteMeds = $scope.pastMedications.concat($scope.futureMedications);
 
-      $scope.futureMedications = _.sortBy(
-      _.filter(ctl.medications, function(item) {
-        var med = moment(item.time);
+      _.forEach(incompleteMeds, function(med) {
+        var medTime = moment(med.time);
 
-        return med.diff(now, 'minutes') >= 0;
-      }), 'time');
+        if (medTime.isSame(now)) {
+          upcomingMedModal(med.name, med.dosage);
+        }
+
+        if (medTime.diff(now, 'minutes') === -5) {
+          missedMedModal(med.name, med.dosage);
+        }
+      });
 
       $scope.$apply();
     }, 1000);
